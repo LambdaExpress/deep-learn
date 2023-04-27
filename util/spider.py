e08d3aa89e17bf6e87db363ff1fa96b5c05770b9
Mr.Z <2838894779@qq.com>
2023-04-27 17:23:45 +0800

    modified:   util/spider.py     日榜->月榜

--- util/spider.py
+++ util/spider.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     spider = Spider(requests.Session())
     try:
         for i in tqdm(range(1, 11), desc="Total Progress"):
-            rank_url = f'https://www.pixiv.net/ranking.php?mode=daily&content=illust&date=20230210&p={str(i)}&format=json'
+            rank_url = f'https://www.pixiv.net/ranking.php?mode=monthly&content=illust&date=20220425&p={str(i)}&format=json'
             work_url_head = r'https://www.pixiv.net/artworks/'
             json = spider.get_web(rank_url).json()
             illust_id_list = spider.parse_json(json)
