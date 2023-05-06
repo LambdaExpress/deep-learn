495c3c65d49043adb77e911785c97abf48b8d1f6
Mr.Z <2838894779@qq.com>
2023-05-06 23:34:01 +0800

    modified:   log.txt 	deleted:    logs/ResNet18/events.out.tfevents.1682606282.SK-20210628UIDA.24196.0 	deleted:    logs/ResNet18/events.out.tfevents.1682606622.SK-20210628UIDA.3548.0 	deleted:    logs/ResNet18/events.out.tfevents.1682606642.SK-20210628UIDA.30996.0 	deleted:    logs/ResNet18/events.out.tfevents.1682607323.SK-20210628UIDA.22584.0 	deleted:    logs/ResNet18/events.out.tfevents.1682639567.SK-20210628UIDA.18200.0 	deleted:    logs/ResNet18/events.out.tfevents.1682639585.SK-20210628UIDA.35224.0 	deleted:    logs/ResNet18/events.out.tfevents.1682644329.SK-20210628UIDA.8144.0 	deleted:    logs/ResNet18/events.out.tfevents.1682646509.SK-20210628UIDA.36148.0 	deleted:    logs/ResNet18/events.out.tfevents.1682646657.SK-20210628UIDA.35532.0 	deleted:    logs/ResNet18/events.out.tfevents.1682650159.SK-20210628UIDA.35428.0 	deleted:    logs/ResNet18/events.out.tfevents.1682651231.SK-20210628UIDA.36340.0 	deleted:    logs/ResNet18/events.out.tfevents.1682651818.SK-20210628UIDA.2224.0 	deleted:    logs/ResNet18/events.out.tfevents.1682749266.SK-20210628UIDA.13924.0 	deleted:    logs/ResNet18/events.out.tfevents.1682772568.SK-20210628UIDA.23752.0 	deleted:    logs/ResNet18/events.out.tfevents.1682772639.SK-20210628UIDA.18596.0 	deleted:    logs/ResNet18/events.out.tfevents.1682777918.SK-20210628UIDA.21232.0 	deleted:    logs/ResNet18/events.out.tfevents.1682778143.SK-20210628UIDA.25072.0 	modified:   train.py 	modified:   utils/txt2excel.py

--- train.py
+++ train.py
@@ -1,79 +1,118 @@
+import os
+import time
+import warnings
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from my_dataset import MyDataset
-import time
-from tqdm import tqdm
-import config
 from torch.utils.tensorboard import SummaryWriter
+from PIL import ImageFile
+from contextlib import contextmanager
+from my_dataset import MyDataset
 from models import resnet18_with_softmax as resnet
-import os
+import config
+from tqdm import tqdm
 
-def train(model : nn.Module, optimizer : optim.Optimizer, criterion, train_loader, i):
-    model.train() 
-    running_loss = 0.0
-    correct = 0.0
+warnings.filterwarnings("ignore")
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+@contextmanager
+def open_log_file(file_name):
+    """
+    A context manager that opens and closes the log file.
+    """
+    with open(file_name, 'a+') as f:
+        yield f
+    f.close()
+
+def train_and_test(model, optimizer, criterion, train_loader, test_loader, epoch):
+    """
+    Trains and tests the model for one epoch.
+
+    Args:
+        model: The ResNet18 model.
+        optimizer: The optimizer used for training the model.
+        criterion: The loss function used for training and testing the model.
+        train_loader: The data loader used for training the model.
+        test_loader: The data loader used for testing the model.
+        epoch: The current epoch number.
+
+    Returns:
+        The training loss, training accuracy, testing loss, and testing accuracy for the epoch.
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    train_loss = 0
+    train_correct = 0
+    train_total = 0
     pars = tqdm(train_loader, leave=False)
     for inputs, labels in pars:
         inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, labels)
-        pars.set_description(f"Training loss {loss:.4f}")
+        pars.set_description(f"Epoch:{epoch + 1} Training Loss {loss.item():.3f}")
         loss.backward()
         optimizer.step()
-        _, preds = torch.max(outputs, 1)
-        correct += torch.sum(preds == labels.data)
-        running_loss += loss.item() * inputs.size(0)
-    epoch_loss = running_loss / len(train_loader.dataset) 
-    epoch_acc = correct.double() / len(train_loader.dataset)
-    writer.add_scalar('train_loss', epoch_loss, i)
-    writer.add_scalar('train_acc', epoch_acc, i)
-    return epoch_loss, epoch_acc
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        train_total += labels.size(0)
+        train_correct += predicted.eq(labels).sum().item()
 
-def test(model : nn.Module, criterion, test_loader, i):
+    train_loss /= len(train_loader)
+    train_acc = train_correct / train_total
+
     model.eval()
-    running_loss = 0.0
-    correct = 0
-    with torch.no_grad(): 
+    test_loss = 0
+    test_correct = 0
+    test_total = 0
+    with torch.no_grad():
         pars = tqdm(test_loader, leave=False)
         for inputs, labels in pars:
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            pars.set_description(f"Testing loss {loss:.4f}")
-            running_loss += loss.item() * inputs.size(0)
-            _, preds = torch.max(outputs, 1)
-            correct += torch.sum(preds == labels.data)
-    epoch_loss = running_loss / len(test_loader.dataset)
-    epoch_acc = correct.double() / len(test_loader.dataset)
-    writer.add_scalar('test_loss', epoch_loss, i)
-    writer.add_scalar('test_acc', epoch_acc, i)
-    return epoch_loss, epoch_acc
-train_loader, test_loader = MyDataset().get_loader()
-writer = SummaryWriter('logs/ResNet18')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = resnet.Resnet18WithSoftmax().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEITHT_DECAY)
+            pars.set_description(f"Epoch:{epoch + 1} Testing Loss {loss.item():.3f}")
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            test_total += labels.size(0)
+            test_correct += predicted.eq(labels).sum().item()
 
-from PIL import ImageFile 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+    test_loss /= len(test_loader)
+    test_acc = test_correct / test_total
 
-fine_tuning = False
-if fine_tuning:
-    model.load_state_dict(torch.load(r"checkpoint/ResNet18_Epoch_94.pth")) 
-print(f'Fine Tuning : {fine_tuning}')
+    return train_loss, train_acc, test_loss, test_acc
 
-t = time.time()
-if os.path.exists(config.LOGTXT_NAME):
-    os.remove(config.LOGTXT_NAME)
-for epoch in range(config.EPOCH):
-    train_loss, train_acc = train(model, optimizer, criterion, train_loader, epoch)
-    test_loss, test_acc = test(model, criterion, test_loader, epoch)
-    log = f"Epoch {epoch+1}/{config.EPOCH} - Train Loss: {train_loss:.4f} - Train Acc: {train_acc:.4f} - Test Loss: {test_loss:.4f} - Test Acc: {test_acc:.4f} - Time: {time.time() - t:.2f}s"
-    with open(config.LOGTXT_NAME, 'a+') as f:
-        f.write(log + "\n")
-    print(log)
-    torch.save(model.state_dict(), f"checkpoint/ResNet18_Epoch_{epoch+1}.pth")
-writer.close()
+def main():
+    train_loader, test_loader = MyDataset().get_loader()
+    writer = SummaryWriter('logs/ResNet18')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = resnet.Resnet18WithSoftmax().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEITHT_DECAY)
+    t = time.time()
+    checkpoint_dir = 'checkpoint'
+
+    fine_tuning = False
+    if fine_tuning:
+        model.load_state_dict(torch.load(r"checkpoint/ResNet18_Epoch_94.pth")) 
+    print(f'Fine Tuning : {fine_tuning}')
+
+    for epoch in range(config.EPOCH):
+        train_loss, train_acc, test_loss, test_acc = train_and_test(model, optimizer, criterion, train_loader, test_loader, epoch)
+        log = f"Epoch {epoch+1}/{config.EPOCH} - Train Loss: {train_loss:.4f} - Train Acc: {train_acc:.4f} - Test Loss: {test_loss:.4f} - Test Acc: {test_acc:.4f} - Time: {time.time() - t:.2f}s"
+        print(log)
+        with open_log_file(config.LOGTXT_NAME) as f:
+            f.write(log + "\n")
+
+        writer.add_scalar('Train Loss', train_loss, epoch)
+        writer.add_scalar('Train Accuracy', train_acc, epoch)
+        writer.add_scalar('Test Loss', test_loss, epoch)
+        writer.add_scalar('Test Accuracy', test_acc, epoch)
+
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        torch.save(model.state_dict(), os.path.join(checkpoint_dir,  f'ResNet18_Epoch_{epoch+1}.pth'))
+
+    writer.close()
+
+if __name__ == '__main__':
+    main()
